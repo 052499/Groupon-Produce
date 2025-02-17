3fd9250828b739c0e271f26cd6296796fcb6f6ea
GreeshmaBorraa <156365934+GreeshmaBorraa@users.noreply.github.com>
2025-02-16 21:11:06 -0600

    adding admin login activity.java files- by Greeshma Borra(S567537)

---
+++ app/src/main/java/com/example/grouponproduceapp/AdminActivity/AdminMainActivity.kt
@@ -0,0 +1,30 @@
+package com.app.growceries.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.app.growceries.R
+import com.app.growceries.databinding.ActivityAdminMainBinding
+
+class AdminMainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityAdminMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding= ActivityAdminMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        NavigationUI.setupWithNavController(binding.bottomMenu, Navigation.findNavController(this,
+            R.id.fragmentContainerView3
+        ))
+
+
+    }
+}
